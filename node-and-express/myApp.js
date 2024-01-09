db41626270772901125a6b0c95b2fd736651ed54
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-09 18:18:40 +0400

    Get Query Parameter Input from the Client

--- node-and-express/myApp.js
+++ node-and-express/myApp.js
@@ -13,33 +13,40 @@ let app = express(); //create an Express app object
 // });
 //----------
 // access static asset styles.css with middleware
-app.use('/public', express.static(__dirname + '/public'));
-//----------
-//Implement a Root-Level Request Logger Middleware
-app.use('/', function (req, res, next) {
-	console.log(`${req.method} ${req.path} - ${req.ip}`);
-	next();
-});
-//----------
-//create simple API route
-app.get('/json', function (req, res) {
-	process.env.MESSAGE_STYLE === 'uppercase'
-		? res.json({ message: 'HELLO JSON' })
-		: res.json({ message: 'Hello json' });
-});
+// app.use('/public', express.static(__dirname + '/public'));
+// //----------
+// //Implement a Root-Level Request Logger Middleware
+// app.use('/', function (req, res, next) {
+// 	console.log(`${req.method} ${req.path} - ${req.ip}`);
+// 	next();
+// });
+// //----------
+// //create simple API route
+// app.get('/json', function (req, res) {
+// 	process.env.MESSAGE_STYLE === 'uppercase'
+// 		? res.json({ message: 'HELLO JSON' })
+// 		: res.json({ message: 'Hello json' });
+// });
 //Chain Middleware to Create a Time Server
-app.get(
-	'/now',
-	function (req, res, next) {
-		req.time = new Date().toString();
-		next();
-	},
-	function (req, res) {
-		res.send({ time: req.time });
-	},
-);
-//Get Route Parameter Input from the Client
-app.get('/:word/echo', function (req, res) {
-	res.send({ echo: req.params.word });
+// app.get(
+// 	'/now',
+// 	function (req, res, next) {
+// 		req.time = new Date().toString();
+// 		next();
+// 	},
+// 	function (req, res) {
+// 		res.send({ time: req.time });
+// 	},
+// );
+// //Get Route Parameter Input from the Client
+// app.get('/:word/echo', function (req, res) {
+// 	res.send({ echo: req.params.word });
+// });
+//Get Query Parameter Input from the Client (http://localhost:3000/name?first=Islam&last=Ghareeb)
+app.get('/name', function (req, res) {
+	let firstName = req.query.first;
+	let lastName = req.query.last;
+	res.json({ name: `${firstName} ${lastName}` });
 });
+
 module.exports = app;
