5c2d109677130ebf3dd5c4d768cb9ef3c283ba85
Islam Ghareeb <58330220+Ighareeb@users.noreply.github.com>
2024-01-11 07:58:20 +0400

    corrections project 3

--- project-urlshortener/index.js
+++ project-urlshortener/index.js
@@ -16,41 +16,38 @@ app.get('/', function (req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-client.connect((err) => {
-	if (err) throw err;
-	const db = client.db('urlshortener');
-	const urls = db.collection('urls');
-	const urlparser = require('url');
-	const dns = require('dns');
-	//set up route handler to POST a URL and get JSON response with 'original_url' and 'short_url' props + redirect to original URL if short URL used + error handling if invalid URL used
-	app.post('/api/shorturl', function (req, res) {
-		console.log(req.body);
-		const url = req.body.url;
-		const dnslookup = dns.lookup(
-			urlparser.parse(url).hostname,
-			async (err, address) => {
-				if (err || !address) {
-					res.json({ error: 'Invalid URL' });
-				} else {
-					const urlCount = await urls.countDocuments({});
-					const urlDoc = {
-						url: url,
-						short_url: urlCount,
-					};
-					const result = await urls.insertOne(urlDoc);
-					console.log(result);
-					res.json({ original_url: url, short_url: urlCount });
-				}
-			},
-		);
-	});
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
+const urlparser = require('url');
+const dns = require('dns');
+//set up route handler to POST a URL and get JSON response with 'original_url' and 'short_url' props + redirect to original URL if short URL used + error handling if invalid URL used
+app.post('/api/shorturl', function (req, res) {
+	console.log(req.body);
+	const url = req.body.url;
+	const dnslookup = dns.lookup(
+		urlparser.parse(url).hostname,
+		async (err, address) => {
+			if (err || !address) {
+				res.json({ error: 'Invalid URL' });
+			} else {
+				const urlCount = await urls.countDocuments({});
+				const urlDoc = {
+					url: url,
+					short_url: urlCount,
+				};
+				const result = await urls.insertOne(urlDoc);
+				console.log(result);
+				res.json({ original_url: url, short_url: urlCount });
+			}
+		},
+	);
+});
 
-	app.get('/api/shorturl/:short_url', async (req, res) => {
-		const shorturl = req.params.short_url;
-		const urlDoc = await urls.findOne({ short_url: +shorturl });
-		res.redirect(urlDoc.url);
-	});
-	app.listen(port, function () {
-		console.log(`Listening on port ${port}`);
-	});
+app.get('/api/shorturl/:short_url', async (req, res) => {
+	const shorturl = req.params.short_url;
+	const urlDoc = await urls.findOne({ short_url: +shorturl });
+	res.redirect(urlDoc.url);
+});
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`);
 });
